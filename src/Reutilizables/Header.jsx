64e5cce40b1bb979d79a63ff83bd11804a50f2c3
Johann Gomez <johanngomez09@gmail.com>
2022-08-29 22:55:24 -0400

    detalles de logo y colores

--- src/Reutilizables/Header.jsx
+++ src/Reutilizables/Header.jsx
@@ -1,18 +1,16 @@
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { MenSup } from "./Botones/MenSup.jsx"
 import "./estReu.css"
 
 export const Header = () => {
   return (
     <div className="headerBox">
+      <div className="logoHelmet">
+      </div>
       <div className="welcomeU">
         <b>Bienvenido, nombre de cliente</b>
       </div>
       <div className="sesion">
-        <ButtonGroup aria-label="Basic example">
-          <Button variant="warning">Perfil</Button>
-          <Button variant="warning">Cerrar sesión</Button>
-        </ButtonGroup>
+        <MenSup />
       </div>
     </div>
   )
