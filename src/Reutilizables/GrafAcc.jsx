ef47cec973e9024de965329dfb7ce46f46f8c498
Johann Gomez <johanngomez09@gmail.com>
2022-11-02 22:09:11 -0300

    cambios pequeños

--- src/Reutilizables/GrafAcc.jsx
+++ src/Reutilizables/GrafAcc.jsx
@@ -1,23 +1,39 @@
-import React from 'react';
-// import { MDBChart, MDBCol } from 'mdb-react-ui-kit';
+import React from 'react'
+import {Line} from 'react-chartjs-2'
+
+const state = {
+  labels: ['January', 'February', 'March',
+           'April', 'May'],
+  datasets: [
+    {
+      label: 'Rainfall',
+      fill: false,
+      lineTension: 0.5,
+      backgroundColor: 'rgba(75,192,192,1)',
+      borderColor: 'rgba(0,0,0,1)',
+      borderWidth: 2,
+      data: [65, 59, 80, 81, 56]
+    }
+  ]
+}
 
 export const GrafAcc = () => {
   return (
     <div>
-        {/* <MDBCol lg='7'>
-      <MDBChart
-        type='line'
-        data={{
-          labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-          datasets: [
-            {
-              label: 'Traffic',
-              data: [2112, 2343, 2545, 3423, 2365, 1985, 987],
+      <Line
+          data={state}
+          options={{
+            title:{
+              display:true,
+              text:'Average Rainfall per month',
+              fontSize:20
             },
-          ],
-        }}
-      />
-    </MDBCol> */}
+            legend:{
+              display:true,
+              position:'right'
+            }
+          }}
+        />
     </div>
   )
 }
